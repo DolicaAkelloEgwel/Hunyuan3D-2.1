700d1bbfd887c22431fe259a28c48342337a3ba5
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2025-11-25 17:37:52 +0000

    those other install parts

--- install-cuda.sh
+++ install-cuda.sh
@@ -7,9 +7,33 @@ sudo cp /var/cuda-repo-wsl-ubuntu-12-6-local/cuda-*-keyring.gpg /usr/share/keyri
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-12-6
 
+# install python3-config
+sudo apt-get install -y python3-dev
+
 # set cuda path
 export CUDA_HOME=/usr/local/cuda
 export PATH=$CUDA_HOME/bin:$PATH
 
+
+# install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+
 # sync
 uv sync
+
+# install requirements
+cd hy3dpaint/custom_rasterizer 
+uv pip install -e . --no-build-isolation
+
+# change dir
+cd ../../hy3dpaint/DifferentiableRenderer
+
+# compile this thing...
+bash compile_mesh_painter.sh
+
+# this is also needed...
+cd ../..
+wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P hy3dpaint/ckpt
+
+
