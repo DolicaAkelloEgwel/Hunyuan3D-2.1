7d975992bc36aeba69b2c6141a456a865de69149
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2025-12-17 15:44:53 +0000

    alternative for python-config

--- hy3dpaint/DifferentiableRenderer/compile_mesh_painter.sh
+++ hy3dpaint/DifferentiableRenderer/compile_mesh_painter.sh
@@ -1,1 +1,4 @@
-c++ -O3 -Wall -shared -std=c++11 -fPIC `python -m pybind11 --includes` mesh_inpaint_processor.cpp -o mesh_inpaint_processor`python3-config --extension-suffix`
+c++ -O3 -Wall -shared -std=c++11 -fPIC \
+  `~/Hunyuan3D-2.1/hunyuan/bin/python -m pybind11 --includes` \
+  mesh_inpaint_processor.cpp \
+  -o mesh_inpaint_processor`~/Hunyuan3D-2.1/hunyuan/bin/python -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))"`
