1c4f0ca1af5cef56827b39d2350fe4af819cd74b
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-04-28 16:36:58 +0100

    patch test

--- setup.sh
+++ setup.sh
@@ -8,7 +8,16 @@ CUDA_MINOR="${CUDA_VERSION##*.}"
 echo $CUDA_MAJOR
 echo $CUDA_MINOR
 
-exit 1
+case "$CUDA_VERSION" in
+  12.4) CUDA_PATCH="0" ;;
+  13.2) CUDA_PATCH="1" ;;
+  *)
+    echo "Unknown full version for $CUDA_VERSION"
+    exit 1
+    ;;
+esac
+
+exit
 
 # now I'm not even sure what's happening
 sudo bash -c 'cat .source >> /etc/apt/sources.list.d/ubuntu.sources'
@@ -26,14 +35,14 @@ sudo apt install -y python3.10 python3.10-venv python3.10-dev build-essential cm
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 python3.10 -m pip --version
 
-# install cuda 12.4 for WSL
+# install cuda for WSL
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-wsl-ubuntu-12-4-local_12.4.0-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-12-4-local_12.4.0-1_amd64.deb
-sudo cp /var/cuda-repo-wsl-ubuntu-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
+wget https://developer.download.nvidia.com/compute/cuda/${CUDA_MAJOR}.${CUDA_MINOR}.0/local_installers/cuda-repo-wsl-ubuntu-${CUDA_MAJOR}-${CUDA_MINOR}-local_${CUDA_MAJOR}.${CUDA_MINOR}.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-${CUDA_MAJOR}--local_${CUDA_MAJOR}.${CUDA_MINOR}.0-1_amd64.deb
+sudo cp /var/cuda-repo-wsl-ubuntu-${CUDA_MAJOR}-${CUDA_MINOR}-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-4
+sudo apt-get -y install cuda-toolkit-${CUDA_MAJOR}-${CUDA_MINOR}
 
 # set cuda path
 echo "CUDA_HOME=/usr/local/cuda" >> .bashrc
@@ -46,7 +55,7 @@ python3.10 -m venv hunyuan
 
 # pip stuff
 pip install --upgrade pip setuptools wheel
-pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu${CUDA_MAJOR}${CUDA_MINOR}
 pip install bpy==4.0 --extra-index-url https://download.blender.org/pypi/
 pip install -r requirements.txt
 
@@ -62,4 +71,4 @@ cd ../..
 wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth -P hy3dpaint/ckpt
 
 # delete the cuda installaer
-rm cuda-repo-wsl-ubuntu-12-4-local_12.4.0-1_amd64.deb
+rm cuda-repo-wsl-ubuntu-*
