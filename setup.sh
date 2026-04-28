afa08586922a17e0cf4f84604ad91caebace03cd
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-04-28 17:01:57 +0100

    mistake

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ python3.10 -m pip --version
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/${CUDA_MAJOR}.${CUDA_MINOR}.${CUDA_PATCH}/local_installers/cuda-repo-wsl-ubuntu-${CUDA_MAJOR}-${CUDA_MINOR}-local_${CUDA_MAJOR}.${CUDA_MINOR}.${CUDA_PATCH}-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-${CUDA_MAJOR}--local_${CUDA_MAJOR}.${CUDA_MINOR}.${CUDA_PATCH}-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-${CUDA_MAJOR}-${CUDA_MINOR}-local_${CUDA_MAJOR}.${CUDA_MINOR}.${CUDA_PATCH}-1_amd64.deb
 sudo cp /var/cuda-repo-wsl-ubuntu-${CUDA_MAJOR}-${CUDA_MINOR}-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-${CUDA_MAJOR}-${CUDA_MINOR}
