409e2bcc302ba315405fa65ec153cbdfdfe49b0f
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2026-04-28 16:51:54 +0100

    trying this

--- setup.sh
+++ setup.sh
@@ -38,8 +38,8 @@ python3.10 -m pip --version
 # install cuda for WSL
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/${CUDA_MAJOR}.${CUDA_MINOR}.0/local_installers/cuda-repo-wsl-ubuntu-${CUDA_MAJOR}-${CUDA_MINOR}-local_${CUDA_MAJOR}.${CUDA_MINOR}.0-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-${CUDA_MAJOR}--local_${CUDA_MAJOR}.${CUDA_MINOR}.0-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/${CUDA_MAJOR}.${CUDA_MINOR}.${CUDA_PATCH}/local_installers/cuda-repo-wsl-ubuntu-${CUDA_MAJOR}-${CUDA_MINOR}-local_${CUDA_MAJOR}.${CUDA_MINOR}.${CUDA_PATCH}-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-${CUDA_MAJOR}--local_${CUDA_MAJOR}.${CUDA_MINOR}.${CUDA_PATCH}-1_amd64.deb
 sudo cp /var/cuda-repo-wsl-ubuntu-${CUDA_MAJOR}-${CUDA_MINOR}-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-${CUDA_MAJOR}-${CUDA_MINOR}
@@ -55,7 +55,13 @@ python3.10 -m venv hunyuan
 
 # pip stuff
 pip install --upgrade pip setuptools wheel
-pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu${CUDA_MAJOR}${CUDA_MINOR}
+
+if [[ "$CUDA_VERSION" = "12.4" ]]; then
+  pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu${CUDA_MAJOR}${CUDA_MINOR}
+else
+  pip install torch torchvision
+fi
+
 pip install bpy==4.0 --extra-index-url https://download.blender.org/pypi/
 pip install -r requirements.txt
 
